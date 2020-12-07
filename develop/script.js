3a9469de93af876b03243d97b529d7965f615847
Pro-Code-art <jayme.coder@gmail.com>
2020-12-07 15:39:36 -0800

    initial commit

---
+++ develop/script.js
@@ -0,0 +1,14 @@
+// Display current date/time
+
+var NowDate = new Date();
+
+var eDisplayDate = document.getElementById('currentDay');
+eDisplayDate.innerHTML = NowDate;
+
+
+// Render rows
+
+function renderRows () {
+
+    
+}
