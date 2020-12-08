004512112dd285e8c3645ee8ce29bed49bf8c47c
Pro-Code-art <jayme.coder@gmail.com>
2020-12-07 19:22:53 -0800

    created militaryHours array to render row colors more accurately

--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,5 @@
 // Display current date/time at top of page
+var newDate = new Date();
 var nowDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 var displayDate = document.getElementById('currentDay');
 displayDate.innerHTML = nowDate;
@@ -6,11 +7,13 @@ displayDate.innerHTML = nowDate;
 // Variables
 var nowMoment = moment();
 var currentTime = nowMoment.format('LT');
-var currentHour = moment().startOf('hour').fromNow(currentTime);
+var currentHour =  newDate.getHours();
+console.log(currentHour);
 
 // Render rows
 var container = $('.container');
 var businessHours = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm' ];
+var militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 function renderRows() {
     for (i=0; i < businessHours.length; i++) {
@@ -19,32 +22,46 @@ function renderRows() {
         row.addClass('row time-block');
         container.append(row);
 
+        // Render business hours on left
         var column = $('<div>').addClass('col-1 hour');
         column.text(businessHours[i]);
 
-        // Sets css classes based on curren time
-        if (currentHour < businessHours[i]) {
-            row.addClass('past');
-        } else if (currentHour == businessHours[i]) {
-            row.addClass('present');
-        } else if (currentHour > businessHours[i]) {
-            row.addClass('future');
-        }
+       // Render row colors
+       for (j=0; j < militaryHours.length; j++) {
+            if (currentHour < militaryHours[j]) {
+                row.addClass('past');
+            } else if (currentHour == militaryHours[j]) {
+                row.addClass('present');
+            } else if (currentHour > militaryHours[j]) {
+                row.addClass('future');
+            }
+       }
+    
         
         // Creates text area and buttons
         var textArea = $('<textarea>').addClass('col-10 description time-block');
         var saveBtn = $('<button>').addClass('saveBtn col-1');
         row.append(column, textArea, saveBtn);
-        var icon = $('<i>').addClass('fa fa-save');
-        saveBtn.append(icon);
+        var saveIcon = $('<i>').addClass('fa fa-save');
+        saveBtn.append(saveIcon);
     }
 }
 
 renderRows();
+    for (i=0; i < militaryHours.length; i++) {
+        // Sets css classes based on current time
+        if (currentHour < militaryHours[i]) {
+            row.addClass('past');
+        } else if (currentHour == militaryHours[i]) {
+            row.addClass('present');
+        } else if (currentHour > militaryHours[i]) {
+            row.addClass('future');
+        }
+    }
+
 
 $('.saveBtn').click(storeNotes);
 
 function storeNotes() {
 
 }
-
